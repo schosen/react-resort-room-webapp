249f2515b1471ae85c958396f47127639db16f5b
Sarah Chosen <sarahchosen@hotmail.co.uk>
2020-05-28 07:52:37 +0100

    content added via contentful

--- src/context.js
+++ src/context.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import items from './data'
+// import items from './data'
+import Client from './Contentful'
+
+
 
 const RoomContext = React.createContext();
 //
@@ -23,21 +26,34 @@ class RoomProvider extends Component {
 
 
     //getData
-    componentDidMount() {
-        // this.getdata
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
-        let maxPrice = Math.max(...rooms.map(item => item.price));
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+            content_type: "beachResortRoom",
+            order: "sys.createdAt"
+        }); 
+        let rooms = this.formatData(response.items);
+        let featuredRooms = rooms.filter((room) => room.featured === true);
+        let maxPrice = Math.max(...rooms.map((item) => item.price));
         let maxSize = Math.max(...rooms.map((item) => item.size));
         this.setState({
-            rooms,
-            featuredRooms,
-            sortedRooms:rooms, 
-            loading:false,
-            price:maxPrice,
-            maxPrice,
-            maxSize
-        })
+          rooms,
+          featuredRooms,
+          sortedRooms: rooms,
+          loading: false,
+          price: maxPrice,
+          maxPrice,
+          maxSize,
+        });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    componentDidMount() {
+        this.getData()
+        
     }
 
     formatData(items) {
